0a45aff74b5366d2c6da099a1da712182708ad2d
shaheenaz <shaheenaz99@gmail.com>
2021-07-06 14:38:28 +0300

    Add

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ server.put('/updateFav',updateHandler)
 
 //mongo
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/drinks', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const drinksSchema = new mongoose.Schema({
     strDrink : String,
